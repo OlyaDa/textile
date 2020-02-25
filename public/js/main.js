d7ea95dbc813095095afddbce8379be7c1bd2977
ola <dashevska.olga@gmail.com>
2020-02-26 00:43:48 +0300

    add finish prj

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,22 @@ $(document).ready(function() {
     $('.popup-link').magnificPopup();
 });
 
+//scroll
+
+$('.nav-link-new').mPageScroll2id();
+
+
 //mobile menu (header)
+
+$(window).scroll(function() {
+    let height = $(window).scrollTop();
+    if(height > 1){
+        $('.header').addClass('color');
+    } else{
+        $('.header').removeClass('color');
+    }
+});
+
 
 $(function(){
     $('.toggle-menu').on('click',function(){
@@ -52,100 +67,244 @@ $('.why-slider').slick({
     nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-right"></span> </button>',
 });
 
-//slider for "reviews"
+//slider for "companies"
 
-$('.reviews-slider').slick({
-    slidesToShow: 2,
+$('.companies-slider').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
     infinite: true,
     arrows: true,
-    prevArrow:'<button class="slick-arrow slick-prev"> <i class="fas fa-long-arrow-alt-left"></i> </button>',
-    nextArrow:'<button class="slick-arrow slick-next"> <i class="fas fa-long-arrow-alt-right"></i> </button>',
+    prevArrow:'<button class="slick-arrow slick-prev"> <span class="icon-left"></span> </button>',
+    nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-right"></span> </button>',
     dots: false,
     autoplay: false,
     responsive: [
         {
             breakpoint: 992,
             settings: {
-                slidesToShow: 3,
-            }
-        },
-
-        {
-            breakpoint: 769,
-            settings: {
                 slidesToShow: 2,
             }
         },
 
         {
-            breakpoint: 461,
+            breakpoint: 577,
             settings: {
                 slidesToShow: 1,
-                autoplay: false,
             }
         },
     ]
 });
 
-//slider for "certificates"
+//slider for "examples"
 
-$('.certificates-slider').slick({
+$('.examples-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     infinite: true,
-    arrows: false,
+    arrows: true,
     dots: false,
     centerMode: true,
-    slickGoTo: 2,
     centerPadding: '0px',
-    prevArrow:'<button class="slick-arrow slick-prev"> <i class="fas fa-long-arrow-alt-left"></i> </button>',
-    nextArrow:'<button class="slick-arrow slick-next"> <i class="fas fa-long-arrow-alt-right"></i> </button>',
+    prevArrow:'<button class="slick-arrow slick-prev"> <span class="icon-left"></span> </button>',
+    nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-right"></span> </button>',
     responsive: [
         {
-            breakpoint: 993,
+            breakpoint: 992,
             settings: {
-                centerPadding: '0px',
-                slidesToShow: 3,
-            }
-        },
-
-        {
-            breakpoint: 769,
-            settings: {
-                centerPadding: '0px',
                 slidesToShow: 2,
+                centerMode: false,
             }
         },
 
         {
-            breakpoint: 411,
+            breakpoint: 577,
             settings: {
                 slidesToShow: 1,
-                arrows: false,
             }
         },
     ]
 });
 
-//scroll
+//slider for "reviews"
 
-$('.nav-link-new').mPageScroll2id();
+$('.reviews-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    dots: false,
+    centerMode: true,
+    centerPadding: '0px',
+    prevArrow:'<button class="slick-arrow slick-prev"> <span class="icon-left"></span> </button>',
+    nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-right"></span> </button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                centerMode: false,
+            }
+        },
 
-//animation
+        {
+            breakpoint: 691,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+    ]
+});
 
-function ready() {
-    if(window.innerWidth <= 992) {
-        AOS.init({
-            disable: true,
-        });
-    }
-    else {
-        AOS.init({
-            disable: false,
-        });
-    }
+//services items
+
+//btn-active1
+//
+// $(function(){
+//     $('#btn-active1').on('click',function(){
+//         $('#active1').addClass("d-none");
+//         $('#active1').removeClass("d-flex");
+//         $( '#cls1' ).removeClass('d-none');
+//         $( '#cls1' ).addClass('d-flex');
+//     });
+// });
+
+$(function(){
+    $('#btn-cls1').on('click',function(){
+        $('#active1').addClass("d-flex");
+        $('#active1' ).removeClass('d-none');
+        $( '#cls1' ).addClass('d-none');
+        $( '#cls1' ).removeClass('d-flex');
+
+        $('#active2').addClass("d-none");
+        $('#active2').removeClass("d-flex");
+        $( '#cls2' ).removeClass('d-none');
+        $( '#cls2' ).addClass('d-flex');
+
+        $('#active3').addClass("d-none");
+        $('#active3').removeClass("d-flex");
+        $( '#cls3' ).removeClass('d-none');
+        $( '#cls3' ).addClass('d-flex');
+
+        $('#active4').addClass("d-none");
+        $('#active4').removeClass("d-flex");
+        $( '#cls4' ).removeClass('d-none');
+        $( '#cls4' ).addClass('d-flex');
+    });
+});
+
+//btn-active2
+
+$(function(){
+    $('#btn-cls2').on('click',function(){
+        $('#active2' ).removeClass('d-none');
+        $('#active2').addClass("d-flex");
+        $( '#cls2' ).addClass('d-none');
+        $( '#cls2' ).removeClass('d-flex');
+
+        $('#active1').addClass("d-none");
+        $('#active1').removeClass("d-flex");
+        $( '#cls1' ).removeClass('d-none');
+        $( '#cls1' ).addClass('d-flex');
+
+        $('#active3').addClass("d-none");
+        $('#active3').removeClass("d-flex");
+        $( '#cls3' ).removeClass('d-none');
+        $( '#cls3' ).addClass('d-flex');
+
+        $('#active4').addClass("d-none");
+        $('#active4').removeClass("d-flex");
+        $( '#cls4' ).removeClass('d-none');
+        $( '#cls4' ).addClass('d-flex');
+    });
+});
+
+// $(function(){
+//     $('#btn-active2').on('click',function(){
+//         $('#active2').addClass("d-none");
+//         $('#active2').removeClass("d-flex");
+//         $( '#cls2' ).removeClass('d-none');
+//         $( '#cls2' ).addClass('d-flex');
+//     });
+// });
+
+//btn-active3
+
+$(function(){
+    $('#btn-cls3').on('click',function(){
+        $('#active3' ).removeClass('d-none');
+        $('#active3').addClass("d-flex");
+        $( '#cls3' ).addClass('d-none');
+        $( '#cls3' ).removeClass('d-flex');
+
+        $('#active4').addClass("d-none");
+        $('#active4').removeClass("d-flex");
+        $( '#cls4' ).removeClass('d-none');
+        $( '#cls4' ).addClass('d-flex');
+
+        $('#active2').addClass("d-none");
+        $('#active2').removeClass("d-flex");
+        $( '#cls2' ).removeClass('d-none');
+        $( '#cls2' ).addClass('d-flex');
+
+        $('#active1').addClass("d-none");
+        $('#active1').removeClass("d-flex");
+        $( '#cls1' ).removeClass('d-none');
+        $( '#cls1' ).addClass('d-flex');
+    });
+});
+
+// $(function(){
+//     $('#btn-active3').on('click',function(){
+//         $('#active3').addClass("d-none");
+//         $('#active3').removeClass("d-flex");
+//         $( '#cls3' ).removeClass('d-none');
+//         $( '#cls3' ).addClass('d-flex');
+//     });
+// });
+
+//btn-active4
+
+$(function(){
+    $('#btn-cls4').on('click',function(){
+        $('#active4' ).removeClass('d-none');
+        $('#active4').addClass("d-flex");
+        $( '#cls4' ).addClass('d-none');
+        $( '#cls4' ).removeClass('d-flex');
+
+        $('#active3').addClass("d-none");
+        $('#active3').removeClass("d-flex");
+        $( '#cls3' ).removeClass('d-none');
+        $( '#cls3' ).addClass('d-flex');
+
+        $('#active2').addClass("d-none");
+        $('#active2').removeClass("d-flex");
+        $( '#cls2' ).removeClass('d-none');
+        $( '#cls2' ).addClass('d-flex');
+
+        $('#active1').addClass("d-none");
+        $('#active1').removeClass("d-flex");
+        $( '#cls1' ).removeClass('d-none');
+        $( '#cls1' ).addClass('d-flex');
+    });
+});
+
+// $(function(){
+//     $('#btn-active4').on('click',function(){
+//         $('#active4').addClass("d-none");
+//         $('#active4').removeClass("d-flex");
+//         $( '#cls4' ).removeClass('d-none');
+//         $( '#cls4' ).addClass('d-flex');
+//     });
+// });
+
+
+// setNameScroll
+
+const nameScroll = document.getElementById('input_order');
+const customTextFormSpan = document.getElementById('customTextForm');
+
+function setNameScroll(namescroll, customTextFormSpanF) {
+
+    nameScroll.value = namescroll;
+    customTextFormSpan.textContent = customTextFormSpanF;
 };
-
-
-ready();
